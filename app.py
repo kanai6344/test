e9ff47eeafcac91ae9a46e3d396df761d0c470a2
kanai6344 <kanaimandal6344@gmail.com>
2025-05-27 23:41:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
-
 @app.route('/')
 def home():
     return "Welcome to Flask Blog App"
-
 if __name__ == '__main__':
     app.run(debug=True)
